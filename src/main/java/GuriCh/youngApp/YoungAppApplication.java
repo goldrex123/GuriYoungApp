7e653a51db62d92fedc5ccd4b01bc7cc20ce3e32
kimjunyoung <goldrex123@naver.com>
2023-02-18 16:06:41 +0900

    initial commit by JY

---
+++ src/main/java/GuriCh/youngApp/YoungAppApplication.java
@@ -0,0 +1,13 @@
+package GuriCh.youngApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoungAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoungAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GuriCh/youngApp/YoungAppApplicationTests.java
@@ -0,0 +1,13 @@
+package GuriCh.youngApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoungAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
